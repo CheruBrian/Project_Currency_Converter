f2f6860cb97012d8a6ec95eeafe53613b2b1c508
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-11-29 12:23:33 +0300

    deleted

--- currency_converter/src/api/testApi.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export async function testAPI() {
-  const response = await axios.get(
-    "https://jsonplaceholder.typicode.com/todos/1"
-  );
-  console.log(response.data);
-}
