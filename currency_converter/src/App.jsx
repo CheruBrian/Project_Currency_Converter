3b48c36f2ac573515eb6ba2e9d0e5c7d43366ccf
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-12-16 16:05:25 +0300

    Updated main.jsx

--- currency_converter/src/App.jsx
+++ currency_converter/src/App.jsx
@@ -1,13 +1,9 @@
-// currency_converter/src/App.jsx 
-import React from 'react';
-import { CurrencyConverter } from './commponets/currencyConverter';
+import React from "react";
+import CurrencyConverter from "./components/CurrencyConverter";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
-      <h1 className="text-3xl font-bold text-blue-700 mb-6">
-        Currency Converter
-      </h1>
+    <div>
       <CurrencyConverter />
     </div>
   );
